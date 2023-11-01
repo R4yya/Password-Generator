ac4495a1e37d2173a0d4e91a06905424a014225d
R4yya <121268707+R4yya@users.noreply.github.com>
2023-11-01 22:34:36 +0300

    Update generate_password.py

--- utils/password_generation/generate_password.py
+++ utils/password_generation/generate_password.py
@@ -1,7 +1,19 @@
-from test_random import generate_random_password
-from test_secrets import generate_secure_password
+from secrets import choice
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
-print(f'random lib, pass: {generate_random_password(10)}')
-print()
 
-print(f'random lib, pass: {generate_secure_password(10)}')
+def generate_secure_password(length, uppercase_statement=True, digits_statement=True, punctuation_statement=True):
+    alphabet = ascii_lowercase
+
+    alphabet += ascii_uppercase if uppercase_statement else ''
+    alphabet += digits if digits_statement else ''
+    alphabet += punctuation if punctuation_statement else ''
+
+    password = ''.join(choice(alphabet) for _ in range(length))
+
+    return password
+
+
+if __name__ == '__main__':
+    secure_password = generate_secure_password(12)
+    print(secure_password)
