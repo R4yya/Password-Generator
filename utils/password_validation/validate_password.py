6fc1a1d5b46eec75278f8b17a4333d2320bc6af7
R4yya <121268707+R4yya@users.noreply.github.com>
2023-11-02 14:05:50 +0300

    Update validate_password.py

--- utils/password_validation/validate_password.py
+++ utils/password_validation/validate_password.py
@@ -14,11 +14,11 @@ def validate_password(password):
     password_length = len(password)
 
     if password_length <= MIN_PASSWORD_LENGTH:
-        errors.append(f'Your password is too short. It only have {password_length} characters.')
+        errors.append(f'Your password is too short. It only has {password_length} characters.')
     elif password_length <= REGULAR_PASSWORD_LENGTH:
-        warnings.append(f'Your password should be longer. It only have {password_length} characters.')
+        warnings.append(f'Your password should be longer. It only has {password_length} characters.')
     else:
-        messages.append(f'Your password is long. In have {password_length} characters.')
+        messages.append(f'Your password is long. In has {password_length} characters.')
 
     def check_and_add_message(regex, message, error):
         if search(regex, password):
@@ -31,21 +31,43 @@ def validate_password(password):
     check_and_add_message(r'[\d]', 'Password contains digits.', 'Password should contain digits.')
     check_and_add_message(f'[{punctuation}]', 'Password contains special characters.', 'Password should contain special characters (e.g., "!@#$?_" and so on).')
 
+    common_passwords = [
+        '123456', 'password', '123456789', '12345678', '12345', '1234567', '1234567890', 'qwerty', 'abc123', '111111',
+        'letmein', 'welcome', 'monkey', '1234', 'sunshine', '123321', 'superman', '1234567', 'iloveyou', 'starwars',
+        'princess', 'password1', 'hello', 'world', 'admin', 'root', '123123', '123', '666666', '1qaz2wsx', '654321',
+        'abc123456', 'passw0rd', '121212', 'dragon', 'sunshine', 'football', '123456a', 'password123', 'letmein123',
+        '55555', 'welcome1', '1234qwer', 'shadow', '987654321', 'jesus', 'sunshine1', 'admin1', 'welcome123',
+        'whatever', 'superman1', '1q2w3e4r', '123456789a', '123qwe', 'qazwsx', '112233', '12121212', '999999', 'michael',
+        'charlie', 'mustang', '123qwerty', 'letmein1', 'mypassword', 'baseball', 'monkey1', 'iloveyou1', 'master',
+        '1qazxsw2', 'sunshine123', '654321a', 'abc1234', '12345a', 'welcome1234', 'jesus1', '1234abcd', '12345678a',
+        'password1234', 'superman123', '1qaz2wsx3', '1234567890a', '1234qwer1234', 'shadow1', '987654321a', '1234567a',
+        'passw0rd1', '555551', '11223344', '123123123', 'a123456', 'michael1', 'charlie1', 'mustang1', '123qwerty1',
+        'letmein12', 'mypassword1', 'baseball1', 'monkey123', 'iloveyou12', 'master1'
+    ]
+
+    if password in common_passwords:
+        errors.append('Your password uses common words')
+    else:
+        messages.append('Your password does not contain common words')
+
     return (messages, warnings, errors)
 
 
 if __name__ == '__main__':
-    messages, warnings, errors = validate_password(input('Enter your password in order t ocheck it: '))
+    messages, warnings, errors = validate_password(input('Enter your password to validate it: '))
+
     if any([messages, warnings, errors]):
-        print('\nSummary:')
         if messages:
+            print('Messages:')
             for message in messages:
                 print(message)
 
         if warnings:
+            print('Warnings:')
             for warning in warnings:
                 print(warning)
 
         if errors:
+            print('Errors:')
             for error in errors:
                 print(error)
